a603a9d93e39c4b01332d033179d9d8d259f358d
Felipe Pelichero <pelichero_13@hotmail.com>
2017-10-03 23:23:08 -0300

    polishing

--- acrp-web/README.md
+++
@@ -1,5 +0,0 @@
-# Spring Boot Web Application
-To run the application
-mvn spring-boot:run 
-after that...
-http://locahost:8080
--- acrp-web/src/main/java/com/br/ac/rp/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.ac.rp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.br.ac.rp.domain.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Integer>{
-}
